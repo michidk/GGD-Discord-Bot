16021869167aa4a14e12b05465fa333915bff162
Michael Lohr <michidk@live.de>
2017-08-26 20:19:00 +0200

    More Initial Stuff

--- src/main/java/net/germangamedevs/Main.java
+++ src/main/java/net/germangamedevs/Main.java
@@ -1,7 +1,58 @@
 package net.germangamedevs;
 
+import com.google.common.io.Resources;
+import de.btobastian.sdcf4j.CommandHandler;
+import de.btobastian.sdcf4j.handler.JDA3Handler;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.germangamedevs.commands.PingCommand;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 /**
- * Created by Michi on 26-Aug-17.
+ * Created by Michael Lohr on 26-Aug-17.
  */
 public class Main {
+
+    private static final String TOKEN_FILE_NAME = "token.txt";
+
+    public static void main(String [] args){
+
+        String token = null;
+
+        try {
+            token = Resources.toString(Resources.getResource(TOKEN_FILE_NAME), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (token == null) {
+            System.err.println("Can't read token.txt. Exit");
+            return;
+        }
+
+        JDA jda = null;
+        try {
+            jda = new JDABuilder(AccountType.BOT).setToken(token).buildBlocking();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (RateLimitedException e) {
+            e.printStackTrace();
+        }
+
+        if (jda == null) {
+            System.err.println("Something went wrong. Exit");
+            return;
+        }
+
+        CommandHandler handler = new JDA3Handler(jda);
+        handler.registerCommand(new PingCommand());
+    }
+
 }
--- src/main/java/net/germangamedevs/commands/PingCommand.java
+++ src/main/java/net/germangamedevs/commands/PingCommand.java
@@ -1,7 +1,16 @@
 package net.germangamedevs.commands;
 
+import de.btobastian.sdcf4j.Command;
+import de.btobastian.sdcf4j.CommandExecutor;
+
 /**
  * Created by Michael Lohr on 26-Aug-17.
  */
-public class PingCommand {
+public class PingCommand implements CommandExecutor {
+
+    @Command(aliases = {"!ping"}, description = "Pong!")
+    public String onCommand(String command, String[] args) {
+        return "Pong!";
+    }
+
 }
